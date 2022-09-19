29a9a838c1bcf77c99ce03e832d0c4d68f217b34
Rezo Joglidze <rezojoglidze7@gmail.com>
2022-09-19 12:51:43 +0400

    Initial Commit

---
+++ SliderButton/SliderButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderButtonApp.swift
+//  SliderButton
+//
+//  Created by Rezo Joglidze on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SliderButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
