f325977b872e07353d28c16b981a3cabdfa2371d
Rezo Joglidze <rezojoglidze7@gmail.com>
2022-09-19 15:44:22 +0400

    Create Home View

--- SliderButton/ContentView.swift
+++ SliderButton/ContentView.swift
@@ -8,63 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    /*
-     //MARK: Tasks
-     1. Users can drag and slide button to right direction
-     2. The button shape will grow horizontally as well
-     3. When we stop dragging the button halway and we relesea our fingers from the screen, button have to back its starting position
-     4. If we slide button at least end of the width, it have to stay there instead of back its starting position.
-     */
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
         ZStack {
-            Color.blue.opacity(0.7).ignoresSafeArea(.all, edges: .all)
-            
-            ZStack {
-                //MARK: Background(static)
-                Capsule()
-                    .fill(.white.opacity(0.2))
-                
-                Capsule()
-                    .fill(.white.opacity(0.2))
-                    .padding(8)
-                
-                //MARK: Call-to-action(static)
-                
-                Text("Get Started")
-                    .font(.system(.title3, design: .rounded))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .offset(x: 20)
-                
-                //MARK: Capsule (Dynamic width)
-                HStack {
-                    Capsule()
-                        .fill(.red.opacity(0.8))
-                        .frame(width: 80)
-                    
-                    Spacer()
-                }
-                //MARK: Circle (Draggable)
-                HStack {
-                    ZStack {
-                        Circle()
-                            .fill(.red.opacity(0.8))
-                        Circle()
-                            .fill(.black.opacity(0.15))
-                            .padding(8)
-                        
-                        Image(systemName: "chevron.right.2")
-                            .font(.system(size: 24, weight: .bold))
-                        
-                    }.foregroundColor(.white)
-                        .frame(width: 80, height: 80, alignment: .center)
-                    
-                    Spacer()
-                }
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
             }
-            .frame(height: 80, alignment: .center)
-            .padding()
         }
     }
 }
@@ -72,5 +24,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
---
+++ SliderButton/HomeView.swift
@@ -0,0 +1,65 @@
+//
+//  HomeView.swift
+//  SliderButton
+//
+//  Created by Rezo Joglidze on 19.09.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  // MARK: - PROPERTY
+  
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  @State private var isAnimating: Bool = false
+  
+  // MARK: - BODY
+  
+  var body: some View {
+    VStack(spacing: 20) {
+      // MARK: - HEADER
+      Spacer()
+      
+      // MARK: - CENTER
+      
+      Text("The time that leads to mastery is dependent on the intensity of our focus.")
+        .font(.title3)
+        .fontWeight(.light)
+        .foregroundColor(.secondary)
+        .multilineTextAlignment(.center)
+        .padding()
+      
+      // MARK: - FOOTER
+      
+      Spacer()
+      
+      Button(action: {
+        withAnimation {
+          isOnboardingViewActive = true
+        }
+      }) {
+        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+          .imageScale(.large)
+        
+        Text("Restart")
+          .font(.system(.title3, design: .rounded))
+          .fontWeight(.bold)
+      } //: BUTTON
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.capsule)
+      .controlSize(.large)
+    } //: VSTACK
+    .onAppear(perform: {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+        isAnimating = true
+      })
+    })
+  }
+}
+
+// MARK: - PREVIEW
+struct HomeView_Previews: PreviewProvider {
+  static var previews: some View {
+    HomeView()
+  }
+}
