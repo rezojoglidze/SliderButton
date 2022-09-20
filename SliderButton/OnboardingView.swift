6e7ac9f2e918aa5258f21183647b6af30caebb02
Rezo Joglidze <rezojoglidze7@gmail.com>
2022-09-20 11:28:25 +0400

    Change Onboarding template

--- SliderButton/OnboardingView.swift
+++ SliderButton/OnboardingView.swift
@@ -8,13 +8,6 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    /*
-     //MARK: Tasks
-     1. Users can drag and slide button to right direction
-     2. The button shape will grow horizontally as well
-     3. When we stop dragging the button halway and we relesea our fingers from the screen, button have to back its starting position
-     4. If we slide button at least end of the width, it have to stay there instead of back its starting position.
-     */
     
     var body: some View {
         ZStack {
@@ -22,46 +15,13 @@ struct OnboardingView: View {
             
             ZStack {
                 //MARK: Background(static)
-                Capsule()
-                    .fill(.white.opacity(0.2))
-                
-                Capsule()
-                    .fill(.white.opacity(0.2))
-                    .padding(8)
                 
                 //MARK: Call-to-action(static)
                 
-                Text("Get Started")
-                    .font(.system(.title3, design: .rounded))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .offset(x: 20)
-                
                 //MARK: Capsule (Dynamic width)
-                HStack {
-                    Capsule()
-                        .fill(Color("ColorRed"))
-                        .frame(width: 80)
-                    
-                    Spacer()
-                }
+             
                 //MARK: Circle (Draggable)
-                HStack {
-                    ZStack {
-                        Circle()
-                            .fill(Color("ColorRed"))
-                        Circle()
-                            .fill(.black.opacity(0.15))
-                            .padding(8)
-                        
-                        Image(systemName: "chevron.right.2")
-                            .font(.system(size: 24, weight: .bold))
-                        
-                    }.foregroundColor(.white)
-                        .frame(width: 80, height: 80, alignment: .center)
-                    
-                    Spacer()
-                }
+                
             }
             .frame(height: 80, alignment: .center)
             .padding()
